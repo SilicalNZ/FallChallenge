07875ed1cabcc80886b6588bb861daca9b655f0d
silical <SilicalNZ@gmail.com>
2020-11-13 17:28:45 +1300

    Basic program design

--- main.py
+++ main.py
@@ -1,16 +1,126 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import List
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Ingredient:
+    def __init__(self, tier):
+        self.tier = tier
+
+    @classmethod
+    def tier_0(cls):
+        return Ingredient(0)
+
+    @classmethod
+    def tier_1(cls):
+        return Ingredient(1)
+
+    @classmethod
+    def tier_2(cls):
+        return Ingredient(2)
+
+    @classmethod
+    def tier_3(cls):
+        return Ingredient(3)
+
+    @classmethod
+    def from_multiple(cls, tier, amount):
+        return [getattr(cls, f"tier_{tier}") for _ in range(amount)]
 
 
-# Press the green button in the gutter to run the script.
+class IngredientInventory:
+    def __init__(self, ingredients: List[Ingredient]):
+        self.ingredients = ingredients
+
+    @classmethod
+    def from_input(cls, inv_0: int, inv_1: int, inv_2: int, inv_3: int):
+        ingredients = []
+        for tier, amount in zip(range(4), (inv_0, inv_1, inv_2, inv_3)):
+            ingredients.extend(Ingredient.from_multiple(tier, amount))
+
+        return cls(ingredients)
+
+
+class Order(IngredientInventory):
+    def __init__(self, ingredients: List[Ingredient], id: int, price: int):
+        self.id = id
+        self.price = price
+        super().__init__(ingredients)
+
+    @classmethod
+    def from_input(cls, delta_0: int, delta_1: int, delta_2: int, delta_3: int, id: int, price: int):
+        return cls(
+            IngredientInventory.from_input(
+                delta_0, delta_1, delta_2, delta_3
+            ).ingredients,
+            id,
+            price
+        )
+
+
+class OrderInventory:
+    def __init__(self, recipes: List[Order]):
+        self.recipes = recipes
+
+    @classmethod
+    def from_input(cls):
+        actions = []
+        action_count = int(input())  # the number of spells and recipes in play
+        for i in range(action_count):
+            action_id, action_type, \
+            delta_0, delta_1, delta_2, delta_3, \
+            price, tome_index, tax_count, \
+            castable, repeatable = input().split()
+
+            action_id = int(action_id)
+            delta_0 = int(delta_0)
+            delta_1 = int(delta_1)
+            delta_2 = int(delta_2)
+            delta_3 = int(delta_3)
+            price = int(price)
+            tome_index = int(tome_index)
+            tax_count = int(tax_count)
+            castable = castable != "0"
+            repeatable = repeatable != "0"
+
+            actions.append(Order.from_input(delta_0, delta_1, delta_2, delta_3, action_id, price))
+        return cls(actions)
+
+
+class Player(IngredientInventory):
+    def __init__(self, inventory: List[Ingredient], score: int):
+        super().__init__(inventory)
+        self.score = score
+
+    @classmethod
+    def from_input(cls, inv_0: int, inv_1: int, inv_2: int, inv_3: int, score: int):
+        return cls(
+            IngredientInventory.from_input(
+                inv_0, inv_1, inv_2, inv_3
+            ).ingredients,
+            score
+        )
+
+
+class Thinker:
+    def __init__(self, player1, player2, orders: Order):
+        self.perspective = player1
+        self.opponent = player2
+        self.orders = orders
+
+    @classmethod
+    def from_input(cls):
+        order = OrderInventory.from_input()
+        players = []
+        for i in range(2):
+            # inv_0: tier-0 ingredients in inventory
+            # score: amount of rupees
+            players.append([int(j) for j in input().split()])
+        return cls(*players, order)
+
+    @classmethod
+    def run(cls):
+        while True:
+            cls.from_input()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    Thinker.run()
